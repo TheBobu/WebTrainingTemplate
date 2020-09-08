80482455404ea49b107b321f9c89b1f99bd3a7ab
Andrei Florescu <andrei.florescu@itperspectives.ro>
2020-09-08 13:11:31 +0300

    Added Basic Data providers
    - EntityProvider interface and class implementation
    - EntityProfile used for mapping between Entities and Dtos
    - Added AutoMapper dependency
    - DatabaseProvider class with basic functions for executing queries

--- WebTrainingTemplate.DataProviders/src/Services/IEntityProvider.cs
+++ WebTrainingTemplate.DataProviders/src/Services/IEntityProvider.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WebTrainingTemplate.Data.src.Entities;
 
 namespace WebTrainingTemplate.DataProviders.src.Services
 {
     public interface IEntityProvider
     {
+        Entity Delete(int id);
+        Entity GetById(int id);
+        int GetNumberOfEmployees(string info = "");
+        IEnumerable<Entity> GetRange(int nrOfItems, int pageNumber, string info = "");
+        int Insert(Entity newEntity);
+        int Update(Entity updatedEntity);
     }
 }
